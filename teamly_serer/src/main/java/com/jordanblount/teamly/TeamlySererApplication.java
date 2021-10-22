e9759b423c238cece3271312b263ad82b4093036
Jordan Blount <77266395+JordanBlount@users.noreply.github.com>
2021-10-22 11:20:55 -0300

    Created Spring Boot server

---
+++ teamly_serer/src/main/java/com/jordanblount/teamly/TeamlySererApplication.java
@@ -0,0 +1,13 @@
+package com.jordanblount.teamly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamlySererApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamlySererApplication.class, args);
+	}
+
+}
---
+++ teamly_serer/src/test/java/com/jordanblount/teamly/TeamlySererApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jordanblount.teamly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamlySererApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
