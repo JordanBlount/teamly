dc9e0ecbaf9a6319677a758a8155257142f23cf5
Jordan Blount <77266395+JordanBlount@users.noreply.github.com>
2021-10-25 21:44:27 -0400

    Changed Drawer component to be better for desktop and mobile.

--- src/components/DashboardLayout.js
+++ src/components/DashboardLayout.js
@@ -1,69 +1,143 @@
+import { useState } from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import AppBar from '@mui/material/AppBar';
 import CssBaseline from '@mui/material/CssBaseline';
 import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
 import Typography from '@mui/material/Typography';
 import ListItem from '@mui/material/ListItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
+import IconButton from '@mui/material/IconButton';
 import Dashboard from './Dashboard';
+
+import Helmet from 'react-helmet';
+
+import MenuIcon from '@mui/icons-material/Menu';
 
 import Logo from '../resources/logo.svg';
 
-const drawerWidth = 200;
+const drawerWidth = 240;
 
-const DashboardLayout = () => {
+function DashboardLayout(props) {
+
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <div>
+      {/* TODO: Remove this "Toolbar" componenet and replace it with the app title on large
+                      screen sizes. */}
+      <Toolbar>
+        Jordan Blount
+        {/* 
+        Teamly Logo
+      */}
+        {/* <img src={Logo} alt='Teamly Logo' className='logo' width='32' height='32' style={{ marginRight: '1.5rem' }} /> */}
+      </Toolbar>
+
+      <Divider />
+      <List>
+        <ListItem button key={"Teams"}>
+          <ListItemIcon sx={{ minWidth: '32px' }}>
+            <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
+            </svg>
+          </ListItemIcon>
+          <ListItemText primary={"Teams"} />
+        </ListItem>
+        <ListItem button key={"Employees"}>
+          <ListItemIcon sx={{ minWidth: '32px' }}>
+            <svg width='20' height='20' xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />
+            </svg>
+          </ListItemIcon>
+          <ListItemText primary={"Employees"} />
+        </ListItem>
+      </List>
+    </div>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
   return (
     <Box sx={{ display: 'flex' }}>
+      <Helmet>
+        <title>StrongBuilt - Teamly</title>
+      </Helmet>
       <CssBaseline />
-      <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+      <AppBar
+        position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+        }}
+      >
         <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+
+          {/* 
+                      Teamly Logo
+                    */}
           <img src={Logo} alt='Teamly Logo' className='logo' width='32' height='32' style={{ marginRight: '1.5rem' }} />
-          {/* TODO: Use state to update the title here. It should be built 
-                      like organizationName + whatever else */}
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            StrongBuilt
+
+          {/*
+                      Appbar title
+                    */}
+          <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
+            Teamly
           </Typography>
         </Toolbar>
       </AppBar>
-      <Drawer
-        variant="permanent"
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
-          backgroundColor: '#FFFF00'
-        }}
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders"
       >
-        <Toolbar />
-        <Box sx={{ overflow: 'auto' }}>
-
-          {/* NOTE: My routes are going to go here. Teams, Employees */}
-          <List>
-            <ListItem button key={"Teams"}>
-              <ListItemIcon sx={{ minWidth: '32px' }}>
-                <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
-                </svg>
-              </ListItemIcon>
-              <ListItemText primary={"Teams"} />
-            </ListItem>
-            <ListItem button key={"Employees"}>
-              <ListItemIcon sx={{ minWidth: '32px' }}>
-                <svg width='20' height='20' xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />
-                </svg>
-              </ListItemIcon>
-              <ListItemText primary={"Employees"} />
-            </ListItem>
-          </List>
-        </Box>
-      </Drawer>
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth, backgroundColor: "#F2F2F2" },
+          }}
+        >
+          {drawer}
+        </Drawer>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: 'none', sm: 'block' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth, backgroundColor: "#F2F2F2" },
+          }}
+          open
+        >
+          {drawer}
+        </Drawer>
+      </Box>
       <Dashboard />
     </Box>
   );
-};
+}
 
 export default DashboardLayout;
