c35f096a1915242ed2508d5d0189f8052fc3c040
Jordan Blount <77266395+JordanBlount@users.noreply.github.com>
2021-10-25 22:16:00 -0400

    Setup pages for an individual team or employee

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, useParams } from 'react-router-dom';
 import Box from '@mui/material/Box'
 import Toolbar from '@mui/material/Toolbar';
 
@@ -7,15 +7,26 @@ import Team from '../pages/Team';
 import Employee from '../pages/Employee';
 
 const Dashboard = () => {
+
+    const params = useParams();
+
     return (
         <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
             <Toolbar />
             <Switch>
                 <Route exact path="/">
-                    <Team />
+                    <Team all={true}/>
+                </Route>
+                <Route path='/teams/:id'>
+                    {/* TODO: Add the individual team page here. */}
+                    <Team all={true} id={}/>
                 </Route>
                 <Route exact path="/employees">
-                    <Employee />
+                    <Employee all={true}/>
+                </Route>
+                <Route path='/employees/:id'>
+                    {/* TODO: Add my individual employee page here. */}
+                    <Employee all={false} id={} />
                 </Route>
             </Switch>
         </Box>
